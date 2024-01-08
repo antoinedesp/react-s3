92735d8252c0edb23781cf3c5294d762b15882d2
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2024-01-08 12:59:21 +0100

    refactor link classes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
+
+    const linkClass = (isActive) => {
+        return isActive
+            ? "transition hover:text-blue-700  text-blue-700"
+            : "transition hover:text-blue-700  text-black";
+    };
+
     return (
         <div
             className="backdrop-blur flex space-x-4 rounded-full w-80 mx-auto border border-opacity-50 py-2 px-4 place-content-center">
             <div>
                 <NavLink
                     to="/"
-                    className={(function ({isActive, isPending}) {
-                        return isActive
-                            ? "transition hover:text-blue-700  text-blue-700"
-                            : "transition hover:text-blue-700  text-black";
-                    })}>
+                    className={( ({isActive}) => linkClass(isActive) )}>
                     Acceuil
                 </NavLink>
             </div>
@@ -19,23 +22,15 @@ export default function Navigation() {
             <div>
                 <NavLink
                     to="/login"
-                    className={(function ({isActive, isPending}) {
-                        return isActive
-                            ? "transition hover:text-blue-700 text-blue-700"
-                            : "transition hover:text-blue-700  text-black";
-                    })}>
-                    Login
+                    className={( ({isActive}) => linkClass(isActive) )}>
+                Login
                 </NavLink>
             </div>
 
             <div>
                 <NavLink
                     to="/contact"
-                    className={(function ({isActive, isPending}) {
-                        return isActive
-                            ? "transition hover:text-blue-700  text-blue-700"
-                            : "transition hover:text-blue-700  text-black";
-                    })}>
+                    className={( ({isActive}) => linkClass(isActive) )}>
                     Contact
                 </NavLink>
             </div>
