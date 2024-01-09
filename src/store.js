9ec08b27782c2c1a3a1d9dca33c206b22877187d
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2024-01-09 15:12:26 +0100

    create user store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { pastriesApi } from './services/pastries.js'
 import {authenticationApi} from "./services/authentication.js";
+import user from "./services/user.js";
 
 export const store = configureStore({
     reducer: {
         // Add the generated reducer as a specific top-level slice
         [pastriesApi.reducerPath]: pastriesApi.reducer,
         [authenticationApi.reducerPath]: authenticationApi.reducer,
+        user: user,
     },
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
