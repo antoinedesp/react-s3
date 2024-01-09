bcd380bfb80e434a64252cae375ab1e6e3984274
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2024-01-09 15:12:43 +0100

    use authenticatedcrud external component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,20 @@
 import {useEffect, useState} from "react";
-import {useIsAuthenticatedMutation, useLoginMutation, useLogoutMutation} from "../services/authentication.js";
+import {useIsAuthenticatedMutation, useLoginMutation} from "../services/authentication.js";
+import {AuthenticatedCrud} from "./AuthenticatedCrud.jsx";
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout} from "../services/user.js";
 
 export default function Login() {
+
 
     const [isAuthenticatedMutation, { isLoading: isAuthenticationLoading }] = useIsAuthenticatedMutation();
 
     const [loginMutation, { isLoading: isLoginLoading }] = useLoginMutation();
-    const [logoutMutation, { isLoading: isLogoutLoading }] = useLogoutMutation();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const dispatcher = useDispatch();
+    const isUserAuthenticated = useSelector((state) => state.user.isAuthenticated);
 
     // Method to check if user is authenticated on load
     // Method to try to login
@@ -31,35 +35,22 @@ export default function Login() {
 
             if(response.data.id !== null) {
                 alert('Succès.');
-                setIsAuthenticated(true);
+                dispatcher(login())
                 return;
             }
         });
 
     }
 
-    const logoutAttempt = () => {
-        logoutMutation().then((response) => {
-            if (response.data && response.data.loggedIn === false) {
-                setIsAuthenticated(false);
-            }
-            else {
-                alert('Erreur');
-            }
-        });
-    }
-
     const fetchAuthenticationStatus = () => {
         try {
             isAuthenticatedMutation().then((response) => {
                 if(response.error && response.error.status === 401) {
-                    setIsAuthenticated(false);
-                    return;
+                    dispatcher(logout());
                 }
                 else {
-                    setIsAuthenticated(true);
+                    dispatcher(login());
                 }
-
             });
         } catch(e) {
             console.log(e);
@@ -75,27 +66,17 @@ export default function Login() {
         <div className="flex flex-col gap-4 items-center">
 
 
-            <p className="text-2xl font-bold pt-4 italic">{isAuthenticated ? <>Backoffice</> : <>Authentification</>}</p>
+            <p className="text-2xl font-bold pt-4 italic">{isUserAuthenticated ? <>Backoffice</> : <>Authentification</>}</p>
 
             <div>
                 {
-                    isAuthenticated
-                        ? (
-                            <div className="grid grid-rows-1 gap-8">
-
-                                <button
-                                    className="w-48 text-center inline-block rounded-full bg-blue-500 color-white px-8 py-2 text-white hover:bg-blue-700 transition"
-                                    onClick={logoutAttempt}>
-                                    Déconnexion
-                                </button>
-
-                            </div>
-                        )
+                    isUserAuthenticated
+                        ? <AuthenticatedCrud/>
                         : (
                             <div className="grid grid-rows-1 gap-8">
-                                <input className="w-48 text-center inline-block rounded-full px-8 py-2 transition border"
+                                <input placeholder="Email" className="w-48 text-center inline-block rounded-full px-8 py-2 transition border"
                                        type="text" onChange={(e) => setEmail(e.target.value)}/>
-                                <input className="w-48 text-center inline-block rounded-full px-8 py-2 transition border"
+                                <input placeholder="Password" className="w-48 text-center inline-block rounded-full px-8 py-2 transition border"
                                        type="password" onChange={(e) => setPassword(e.target.value)}/>
                                 <button
                                     className="w-48 text-center inline-block rounded-full bg-blue-500 color-white px-8 py-2 text-white hover:bg-blue-700 transition"
@@ -110,3 +91,4 @@ export default function Login() {
 
         </div>);
 }
+
