af13ed33ddab034475a05d1360603dbdc4017175
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2024-01-10 17:34:12 +0100

    add edit form and create form

--- src/pages/AuthenticatedCrud.jsx
+++ src/pages/AuthenticatedCrud.jsx
@@ -1,14 +1,77 @@
 import {useGetAllPastriesQuery} from "../services/pastries.js";
-import {useLogoutMutation} from "../services/authentication.js";
+import {
+    useLogoutMutation,
+    useDeletePastryMutation,
+    useAddPastryMutation,
+    useEditPastryMutation
+} from "../services/authentication.js";
 import {useState} from "react";
 import {logout} from "../services/user.js";
 import {useDispatch} from "react-redux";
 export function AuthenticatedCrud() {
 
-    const { data: pastriesData, error: pastriesError, isLoading: pastriesAreLoading } = useGetAllPastriesQuery();
+    const { refresh: fetchAllPastries, data: pastriesData, error: pastriesError, isLoading: pastriesAreLoading } = useGetAllPastriesQuery();
+
     const [logoutMutation, { isLoading: isLogoutLoading }] = useLogoutMutation();
+    const [addPastryMutation, { isLoading: isPastryAdditionLoading }] = useAddPastryMutation();
+    const [deletePastryMutation, { isLoading: isPastryDeletionLoading }] = useDeletePastryMutation();
+    const [editPastryMutation, { isLoading: isPastryEditionLoading }] = useEditPastryMutation();
+
+    const [isPastryAddFormShown, setIsPastryAddFormShown] = useState(false);
+    const [isEditingPastry, setIsEditingPastry] = useState(0);
+
+
+    const [newPastryImage, setNewPastryImage] = useState("");
+    const [newPastryName, setNewPastryName] = useState("");
+    const [newPastryQuantity, setNewPastryQuantity] = useState(0);
+
+    const [editingPastryImage, setEditingPastryImage] = useState("");
+    const [editingPastryName, setEditingPastryName] = useState("");
+    const [editingPastryQuantity, setEditingPastryQuantity] = useState(0);
 
     const dispatcher = useDispatch();
+
+    const forceRefresh = () => {
+        fetchAllPastries();
+    }
+
+    const addPastrieAttempt = () => {
+        addPastryMutation({
+            image: newPastryImage,
+            name: newPastryName,
+            quantity: newPastryQuantity,
+            choice: '',
+        }).then((response) => {
+            if(response.error) {
+                alert('Erreur')
+            }
+            else if(response.data) {
+                alert('Succès');
+                forceRefresh();
+            }
+        })
+    }
+
+    const editPastryAttempt = (pastrieId) => {
+        editPastryMutation({
+            id: pastrieId,
+            image: editingPastryImage,
+            name: editingPastryName,
+            quantity: editingPastryQuantity,
+            choice: '',
+        }).then((response) => {
+            if(response.error) {
+                alert('Erreur')
+                setIsEditingPastry(0);
+            }
+            else if(response.data) {
+                alert('Succès');
+                forceRefresh();
+                setIsEditingPastry(0);
+            }
+        })
+    }
+
     const logoutAttempt = () => {
         logoutMutation().then((response) => {
             if (response.data && response.data.loggedIn === false) {
@@ -21,14 +84,87 @@ export function AuthenticatedCrud() {
     }
 
     const deletePastrieAttempt = (pastrieId) => {
+
         if(confirm('Êtes vous sur de vouloir supprimer cet élément ?')) {
-            // TODO
+
+            deletePastryMutation(pastrieId).then((response) => {
+
+               if(response.error) {
+
+                   alert('Erreur');
+
+               }
+               else {
+
+                   if(response.data && response.data.status === 200) {
+
+                       alert('Supprimé avec succès');
+                       forceRefresh();
+                   }
+               }
+
+            });
         }
     }
 
+    const showEditionForm = (pastrie) => {
+        setEditingPastryImage(pastrie.image);
+        setEditingPastryName(pastrie.name);
+        setEditingPastryQuantity(pastrie.quantity);
+        setIsEditingPastry(pastrie.id);
+    }
+
     return (<div className="grid grid-rows-1 gap-8">
+        <button
+            className="w-48 text-center inline-block rounded-full bg-blue-500 color-white px-8 py-2 text-white hover:bg-blue-700 transition"
+            onClick={() => setIsPastryAddFormShown(true)}>
+            Ajouter une patisserie
+        </button>
+        {
+            isPastryAddFormShown ? (
+                <>
+                    <input placeholder="URL Image" className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" onChange={(e) => setNewPastryImage(e.target.value)} type="text"/>
+                    <input placeholder="Titre" className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" onChange={(e) => setNewPastryName(e.target.value)} type="text"/>
+                    <input placeholder="Quantité" className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" onChange={(e) => setNewPastryQuantity(e.target.value)} type="number"/>
+                    <button
+                        className="w-48 text-center inline-block rounded-full bg-blue-500 color-white px-8 py-2 text-white hover:bg-blue-700 transition"
+                        onClick={addPastrieAttempt}
+                        >
+                        Ajouter
+                    </button>
+                </>) : <></>
+        }
+
         {pastriesData && pastriesData.map((pastrie) => {
-            return (
+            return isEditingPastry !== 0 && isEditingPastry === pastrie.id ? (
+                    <div key={pastrie.id * Date.now()} className="border-2 max-h-56 h-56 grid grid-cols-12">
+                        <div className="col-span-4 overflow-hidden">
+                            <img src="https://picsum.photos/200/300" className="object-cover w-full"
+                                 alt={`Image of ${pastrie.name}`}/>
+                        </div>
+                        <div className="col-span-8 p-4">
+                            <div
+                                className="flex flex-col gap-2 place-items-center align-middle justify-items-center">
+                                <input defaultValue={editingPastryImage} onChange={event => setEditingPastryImage(event.target.value)} className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" type="text"/>
+                                <input defaultValue={editingPastryName} onChange={event => setEditingPastryName(event.target.value)} className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" type="text"/>
+                                <input defaultValue={editingPastryQuantity} onChange={event => setEditingPastryQuantity(event.target.value)} className="w-48 text-center inline-block rounded-full px-8 py-2 transition border" type="number"/>
+                                <div
+                                    className="flex flex-row space-x-4">
+                                    <div
+                                        onClick={() => editPastryAttempt(pastrie.id)}
+                                        className="w-32 text-center inline-block rounded-full bg-slate-400 color-white px-8 py-2 text-white hover:bg-slate-600 transition">
+                                        Sauvegarder
+                                    </div>
+                                    <div
+                                        className="w-32 text-center inline-block rounded-full bg-red-400 color-white px-8 py-2 text-white hover:bg-red-600 transition"
+                                        onClick={() => deletePastrieAttempt(pastrie.id)}>
+                                        Supprimer
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ) :
                 <div key={pastrie.id * Date.now()} className="border-2 max-h-36 h-36 grid grid-cols-12">
                     <div className="col-span-4 overflow-hidden">
                         <img src="https://picsum.photos/200/300" className="object-cover w-full"
@@ -42,21 +178,16 @@ export function AuthenticatedCrud() {
                             <div
                                 className="text-sm text-slate-400">Quantité: {pastrie.quantity}</div>
                             <div
+                                onClick={() => showEditionForm(pastrie)}
                                 className="flex flex-row space-x-4">
                                 <div
                                     className="w-32 text-center inline-block rounded-full bg-slate-400 color-white px-8 py-2 text-white hover:bg-slate-600 transition">
                                     Modifier
                                 </div>
-                                <div
-                                    className="w-32 text-center inline-block rounded-full bg-red-400 color-white px-8 py-2 text-white hover:bg-red-600 transition"
-                                    onClick={() => deletePastrieAttempt(pastrie.quantity)}>
-                                    Supprimer
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            )
         })}
         <button
             className="w-48 text-center inline-block rounded-full bg-blue-500 color-white px-8 py-2 text-white hover:bg-blue-700 transition"
